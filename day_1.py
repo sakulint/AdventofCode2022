019cd090c7863b76b147423cbbb492651f53f898
sakulint <119583508+sakulint@users.noreply.github.com>
2022-12-01 23:19:36 +0200

    Add files via upload
    day 1

---
+++ day_1.py
@@ -0,0 +1,27 @@
+"""
+Advent of code 2022. Day 1
+"""
+
+def main():
+
+    file = open("input_file", mode="r")
+    file_lines = [line[:-1] for line in file]
+
+    summa = 0
+    lista_summista = []
+
+    for file_line in file_lines:
+
+        if file_line == "":
+            lista_summista.append(summa)
+            summa = 0
+        else:
+            summa += int(file_line)
+
+    file.close()
+
+    lista_summista.sort(reverse=True)
+    print(lista_summista[0])
+
+if __name__ == "__main__":
+    main()
